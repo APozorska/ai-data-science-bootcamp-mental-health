7c31cf98a08a4f5fd2c66ac36872766c4a3782d4
Aleksandra Pozorska <a.pozorska9@gmail.com>
2025-06-02 20:05:54 +0200

    Move flagging logic to seperate module with InconsistencyFlagger

--- depression_classification/preprocessing/data_checks.py
+++ depression_classification/preprocessing/data_checks.py
@@ -20,33 +20,6 @@ def handle_duplicates(df: pd.DataFrame, logger) -> pd.DataFrame:
     return df
 
 
-def add_inconsistency_flags(df: pd.DataFrame, logger) -> pd.DataFrame:
-    """
-    Add single inconsistency flags based on logical conditions and log the process.
-    Args:
-        df (pd.DataFrame): Input DataFrame.
-        logger (logging.Logger): Logger object for logging info.
-    Returns:
-        pd.DataFrame: DataFrame with an 'inconsistency_flag' column.
-    """
-    df['inconsistency_flag'] = (
-            ((df['work_pressure'] > 0) & (df['academic_pressure'] > 0)).astype(int) +
-            ((df['study_satisfaction'] > 0) & (df['job_satisfaction'] > 0)).astype(int) +
-            (
-                    ((df['occupation_status'] == 'Working Professional') & (df['profession'] == 'Student')) |
-                    ((df['occupation_status'] == 'Student') & (df['profession'] != 'Student') & (df['profession'].notna()))
-            ).astype(int)
-    )
-
-    n_inconsistent = df['inconsistency_flag'].sum()
-    logger.info(f"Inconsistency flags count: {n_inconsistent}.")
-    logger.debug(
-        "Observations with inconsistency flag:\n"
-        f"{df[df.inconsistency_flag > 0].to_string()}"
-    )
-    return df
-
-
 def remove_unnecessary_columns(df: pd.DataFrame, logger, columns_to_remove=None) -> pd.DataFrame:
     """
     Remove unnecessary columns from the DataFrame and log the process.
