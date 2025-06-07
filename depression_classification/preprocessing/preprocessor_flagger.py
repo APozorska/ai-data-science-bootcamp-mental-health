871b5ab427d4e7b05ff9da3820da99d3bdc347e2
Aleksandra Pozorska <a.pozorska9@gmail.com>
2025-06-07 19:00:04 +0200

    Add type hints

--- depression_classification/preprocessing/preprocessor_flagger.py
+++ depression_classification/preprocessing/preprocessor_flagger.py
@@ -1,9 +1,9 @@
 from depression_classification.preprocessing.custom_transformers.flagging import ConditionalFlagger, InconsistencyFlagger
 
 
-def get_conditional_flagger(flagging_map):
+def get_conditional_flagger(flagging_map: dict) -> tuple[str, ConditionalFlagger]:
     return "conditional_flagger", ConditionalFlagger(flagging_map)
 
 
-def get_inconsistency_flagger():
+def get_inconsistency_flagger() -> tuple[str, InconsistencyFlagger]:
     return "inconsistency_flagger", InconsistencyFlagger()
